1954825db718eba28d7fecbe4727d9f6b850f11f
Yeren Palacios <yeren.palacios@omnilatam.com>
2023-06-24 11:05:25 -0500

    added style changes

--- src/components/Post/simplePost/simplePost.tsx
+++ src/components/Post/simplePost/simplePost.tsx
@@ -14,7 +14,7 @@ export function SimplePost({ data, type='' }:{data: Post, type?: string}) {
     }
     return (
         <div onClick={setCurrentPost} className={`simple-post ${type}`}>
-            <img src={data.images && data.images[0].image} />
+            <img loading='lazy' src={data.images && data.images[0].image} />
             <div className="hover-data">
                 <p>{icons.like_svg} {data.likes_count}</p>
                 <p>{icons.comment} {data.comments_count}</p>
